084b4df153289dac2272026c6f84e9e2b0df4e01
metadirective <schneo@gmail.com>
2018-02-25 12:29:59 +0100

    test change

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-print "a first test"
-print "test with @username et push"
-print "ligne 3"
-print "16h49 lundi"
-print "from laptop 11h08 12/12"
-print "13 11h55"
-print "13h40"
-print "14h19 from XP"
-print "test from win7 box H"
-print "test again"
-print "test again2"
-print "test again3"
+print("new test 12h27")
+
